f298f52887659475aa1e3b6a5811cd757e032fa5
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-07-09 00:58:48 +0300

    wip

--- src/ISteamApps/GetAppList.rs
+++ src/ISteamApps/GetAppList.rs
@@ -9,6 +9,10 @@ pub struct SteamApp {
 
 use serde_json::Value;
 use crate::util;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
 pub fn get() -> Vec<SteamApp> {
     let api_response = make_api_call();
@@ -30,7 +34,23 @@ pub fn make_api_call() -> String {
     let response = minreq::get(url).send();
     let raw_response : Vec<u8> = response.unwrap().into_bytes();
     let response_string = String::from_utf8(raw_response).unwrap();
-    // println!("{}", response_string);
+
+    let path = "cache";
+    let resource = [interface, method, version, "json"].join("-");
+
+    let mut file: File;
+    let filepath = [path, "/", &resource].join("");
+
+
+    let directory_exists = Path::new(path).is_dir();
+    if !directory_exists {
+        fs::create_dir(path).unwrap();
+        file = File::create(filepath).unwrap();
+    } else {
+        file = File::create(filepath).unwrap();
+    }
+
+    file.write_all(response_string.as_ref()).unwrap();
 
     response_string
 }
@@ -39,10 +59,8 @@ pub fn parse_api_call_result(response_string: String) -> Vec<SteamApp> {
     let mut json: Value = serde_json::from_str(&response_string).unwrap();
 
     let mut applist = json["applist"].take();
-    // println!("{}, applist", applist);
 
     let mut apps : Value = applist["apps"].take();
-    // println!("{}, apps", apps);
 
     let list : Vec<SteamApp> = serde_json::from_value(apps).unwrap();
 
@@ -50,7 +68,6 @@ pub fn parse_api_call_result(response_string: String) -> Vec<SteamApp> {
                             .into_iter()
                             .filter(|steam_app| steam_app.name != "")
                             .collect();
-    // println!("{}, apps number", list.len());
 
     filtered_list
 }
