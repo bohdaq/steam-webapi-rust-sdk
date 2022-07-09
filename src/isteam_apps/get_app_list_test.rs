51e58fa72d42e6f65490ab2cfc65b29b29007db8
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-07-09 20:36:54 +0300

    wip

--- src/isteam_apps/get_app_list_test.rs
+++ src/isteam_apps/get_app_list_test.rs
@@ -2,6 +2,8 @@ use crate::isteam_apps::get_app_list::make_api_call;
 use crate::isteam_apps::get_app_list::parse_api_call_result;
 use crate::isteam_apps::get_app_list::get;
 use crate::isteam_apps::get_app_list::get_cached;
+use crate::isteam_apps::get_app_list::get_resource_filepath;
+use std::fs::metadata;
 
 #[test]
 fn test_make_api_call() {
@@ -24,6 +26,12 @@ fn test_parse_api_call_result() {
 
 #[test]
 fn test_get() {
+    let mut cache_timestamp = 0;
+    let boxed_metadata = metadata(get_resource_filepath());
+    if boxed_metadata.is_ok() {
+        cache_timestamp = boxed_metadata.unwrap().modified().unwrap().elapsed().unwrap().as_millis();
+    }
+
     let steam_app_list = get();
 
     assert!(steam_app_list.len()>0);
@@ -31,6 +39,15 @@ fn test_get() {
     assert!(steam_app.appid > 0);
 
     assert!(steam_app.name.len() > 0);
+
+    let mut latest_cache_timestamp = 0;
+    let boxed_metadata = metadata(get_resource_filepath());
+    if boxed_metadata.is_ok() {
+        latest_cache_timestamp = boxed_metadata.unwrap().modified().unwrap().elapsed().unwrap().as_millis();
+    }
+
+    let cache_is_updated = latest_cache_timestamp < cache_timestamp;
+    assert!(cache_is_updated);
 }
 
 #[test]
