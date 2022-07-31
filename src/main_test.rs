3c2e7da7d68ea14f2404ff3bd30699e5da808658
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-08-01 00:45:20 +0300

    wip

--- src/main_test.rs
+++ src/main_test.rs
@@ -10,8 +10,8 @@ fn test_get_app_details() {
 
 #[test]
 fn test_get_cached_app_details() {
-    let app_id = 570;
+    let app_id = 730;
     let app = get_cached_app_details(app_id).unwrap();
 
-    assert_eq!(app.name, "Dota 2".to_string());
+    assert_eq!(app.name, "Counter-Strike: Global Offensive".to_string());
 }
