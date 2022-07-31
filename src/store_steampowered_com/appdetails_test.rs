01907f365c130d42ab4c4bf15099ca46478c454a
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-08-01 01:49:07 +0300

    wip

--- src/store_steampowered_com/appdetails_test.rs
+++ src/store_steampowered_com/appdetails_test.rs
@@ -15,3 +15,19 @@ fn test_get_cached() {
 
     assert_eq!(app.name, "Counter-Strike: Global Offensive".to_string());
 }
+
+#[test]
+fn test_get_resource_filepath() {
+    let app_id = 730;
+    let resource_filepath = appdetails::get_resource_filepath(app_id);
+
+    assert_eq!(resource_filepath, "steam-webapi-cache/steampowered/appdetails/0/730/730.json".to_string());
+}
+
+#[test]
+fn test_get_resource_filepath_long_number() {
+    let app_id = 147730;
+    let resource_filepath = appdetails::get_resource_filepath(app_id);
+
+    assert_eq!(resource_filepath, "steam-webapi-cache/steampowered/appdetails/14/147730/147730.json".to_string());
+}
