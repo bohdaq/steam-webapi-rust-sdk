dcf2e6ef65ac7a050112b7e5f7c13891c297e468
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-08-01 01:46:51 +0300

    wip

--- src/store_steampowered_com/appdetails_test.rs
+++ src/store_steampowered_com/appdetails_test.rs
@@ -1,11 +1,17 @@
 use crate::store_steampowered_com::appdetails;
 
 #[test]
-fn test_make_api_call_details_dota() {
-    let response = appdetails::get(570);
+fn test_get() {
+    let app_id = 570;
+    let app = appdetails::get(app_id).unwrap();
+
+    assert_eq!(app.name, "Dota 2".to_string());
 }
 
 #[test]
-fn test_make_api_call_details_csgo() {
-    let response = appdetails::get(730);
+fn test_get_cached() {
+    let app_id = 730;
+    let app = appdetails::get_cached(app_id).unwrap();
+
+    assert_eq!(app.name, "Counter-Strike: Global Offensive".to_string());
 }
